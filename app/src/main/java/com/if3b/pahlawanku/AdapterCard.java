7bb7cbd133e22213ca9a64d9cd2e94e182d4d90f
BebinPaula <bebinpaula11@gmail.com>
2022-11-14 11:10:47 +0700

    Pahlawanku

---
+++ app/src/main/java/com/if3b/pahlawanku/AdapterCard.java
@@ -0,0 +1,23 @@
+package com.if3b.pahlawanku;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdapterCard {
+
+    public class ClassViewHolder extends RecyclerView.ViewHolder {
+    ImageView ivFoto;
+    TextView tvNama, tvTentang;
+
+        public ClassViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivFoto = itemView.findViewById(R.id.iv_foto);
+            tvNama = itemView.findViewById(R.id.tv_nama);
+            tvTentang = itemView.findViewById(R.id.tv_tentang);
+        }
+    }
+}
---
+++ app/src/main/java/com/if3b/pahlawanku/ModelPahlawan.java
@@ -0,0 +1,31 @@
+package com.if3b.pahlawanku;
+
+public class ModelPahlawan {
+    private String nama;
+    private String tentang;
+    private String foto;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTentang() {
+        return tentang;
+    }
+
+    public void setTentang(String tentang) {
+        this.tentang = tentang;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+}
