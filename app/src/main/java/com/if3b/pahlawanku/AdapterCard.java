b1104211f61605ec85b806a06ce85c30f21bd68a
BebinPaula <bebinpaula11@gmail.com>
2022-11-16 15:11:47 +0700

    UploadAdapterCard

--- app/src/main/java/com/if3b/pahlawanku/AdapterCard.java
+++ app/src/main/java/com/if3b/pahlawanku/AdapterCard.java
@@ -1,13 +1,49 @@
 package com.if3b.pahlawanku;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class AdapterCard {
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class AdapterCard extends RecyclerView.Adapter<AdapterCard.ClassViewHolder>{
+    private ArrayList<ModelPahlawan> dataPahlawan;
+
+    public AdapterCard(ArrayList<ModelPahlawan> dataPahlawan) {
+        this.dataPahlawan = dataPahlawan;
+    }
+
+    @NonNull
+    @Override
+    public ClassViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View varView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_card, parent, false);
+        return new ClassViewHolder(varView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ClassViewHolder holder, int position) {
+    ModelPahlawan pahlawan = dataPahlawan.get(position);
+    holder.tvNama.setText(pahlawan.getNama());
+    holder.tvTentang.setText(pahlawan.getTentang());
+    Glide
+            .with(holder.itemView.getContext())
+            .load(pahlawan.getFoto())
+            .centerCrop()
+            .into(holder.ivFoto);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return dataPahlawan.size();
+    }
 
     public class ClassViewHolder extends RecyclerView.ViewHolder {
     ImageView ivFoto;
--- app/src/main/java/com/if3b/pahlawanku/MainActivity.java
+++ app/src/main/java/com/if3b/pahlawanku/MainActivity.java
@@ -1,14 +1,32 @@
 package com.if3b.pahlawanku;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvPahlawan;
+    private ArrayList<ModelPahlawan> data = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvPahlawan = findViewById(R.id.rv_pahlawan);
+        rvPahlawan.setHasFixedSize(true);
+
+        data.addAll(DataPahlawan.ambilDataPahlawan());
+        tampilDataCard();
+    }
+
+    private  void tampilDataCard(){
+        rvPahlawan.setLayoutManager(new LinearLayoutManager(this));
+        AdapterCard varAdapterCard = new AdapterCard(data);
+        rvPahlawan.setAdapter(varAdapterCard);
     }
 }
